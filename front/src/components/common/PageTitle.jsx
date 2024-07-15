c7fd7ae9d15ad9e2d43ad1b26f1c3d56a88354dc
jidefine <148775766+jidefine@users.noreply.github.com>
2024-07-15 14:29:44 +0900

    fix: 카테고리 classname 수정

--- front/src/components/common/PageTitle.jsx
+++ front/src/components/common/PageTitle.jsx
@@ -1,19 +1,18 @@
 // BasidLayout의 area에서 sidebar를 제외한 콘텐츠 영역의 헤더
-import React from 'react';
-import '../../assets/styles/App.scss';
-
+import React from "react";
+import "../../assets/styles/App.scss";
 
 const PageTitle = ({ main, sub, info }) => {
-    return (
-        <div className="page-title">
-          <div className="catagory-main-sub">
-            <h1>{main}</h1>
-            <div className="category-arrow"></div>
-            <h1>{sub}</h1>
-          </div>
-          <div className='catagory-info'>{info}</div>
-        </div>
-    );
-}
+  return (
+    <div className="page-title">
+      <div className="category-main-sub">
+        <h1>{main}</h1>
+        <div className="category-arrow"></div>
+        <h1>{sub}</h1>
+      </div>
+      <div className="category-info">{info}</div>
+    </div>
+  );
+};
 
 export default PageTitle;
