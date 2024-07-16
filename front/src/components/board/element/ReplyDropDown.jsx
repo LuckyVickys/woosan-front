1e98a59bfe99dd3b76597c34e03892bb48bc8007
jidefine <148775766+jidefine@users.noreply.github.com>
2024-07-16 15:48:17 +0900

    fix; 댓글 작성자만 삭제 가능하도록 수정

--- front/src/components/board/element/ReplyDropDown.jsx
+++ front/src/components/board/element/ReplyDropDown.jsx
@@ -1,23 +1,25 @@
-import React, { useState } from 'react';
-import ReportModal from './ReportModal';
-import MsgModal from './MsgModal';
-import Swal from 'sweetalert2';
+import React, { useState } from "react";
+import ReportModal from "./ReportModal";
+import MsgModal from "./MsgModal";
+import Swal from "sweetalert2";
 import { deleteReply } from "../../../api/replyApi"; // Import deleteReply function
 import { useSelector } from "react-redux";
 import { MdOutlineLocalPostOffice } from "react-icons/md";
-import '../../../assets/styles/App.scss';
+import "../../../assets/styles/App.scss";
 
-const ReplyDropDown = ({ onSelect, replyId, openReport, openMsg, onDeleteSuccess }) => {
+const ReplyDropDown = ({
+    onSelect,
+    replyId,
+    openReport,
+    openMsg,
+    showDeleteButton,
+    onDeleteSuccess,
+}) => {
     const [openReportModal, setOpenReportModal] = useState(false);
     const [openMsgModal, setOpenMsgModal] = useState(false);
     const loginState = useSelector((state) => state.loginSlice);
 
-
     const handleReport = () => {
-        // const id = replyId;
-        // console.log("Send Report id:", id);
-        // onSelect("report", id);
-        // setOpenReportModal(true);
         openReport();
     };
 
@@ -30,26 +32,34 @@ const ReplyDropDown = ({ onSelect, replyId, openReport, openMsg, onDeleteSuccess
         const removeDTO = {
             id: replyId,
             writerId: loginState.id,
-        }
+        };
         Swal.fire({
-            title: '댓글을 삭제하시겠습니까?',
+            title: "댓글을 삭제하시겠습니까?",
             text: `${replyId}를 삭제하시면 복구하실 수 없습니다.`,
-            icon: 'warning',
+            icon: "warning",
             showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: '삭제',
-            cancelButtonText: '취소'
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "삭제",
+            cancelButtonText: "취소",
         }).then((result) => {
             if (result.isConfirmed) {
                 deleteReply(removeDTO)
                     .then(() => {
-                        Swal.fire('삭제 완료', `${replyId}가 삭제되었습니다.`, 'success').then(() => {
+                        Swal.fire(
+                            "삭제 완료",
+                            `${replyId}가 삭제되었습니다.`,
+                            "success"
+                        ).then(() => {
                             onDeleteSuccess(replyId); // 여기에서 상태 업데이트를 호출합니다
                         });
                     })
                     .catch((error) => {
-                        Swal.fire('삭제 실패', '댓글 삭제에 실패했습니다.', 'error');
+                        Swal.fire(
+                            "삭제 실패",
+                            "댓글 삭제에 실패했습니다.",
+                            "error"
+                        );
                         console.error("Error deleting reply:", error);
                     });
             }
@@ -60,18 +70,23 @@ const ReplyDropDown = ({ onSelect, replyId, openReport, openMsg, onDeleteSuccess
         <>
             <div className="comment-dropdown-wrapper">
                 <div className="comment-dropdown-list">
-                    <div className='comment-dropdown' onClick={handleReport}>
-                        <div className='comment-report-icon'></div>
-                        <div className='comment-report-text'>신고하기</div>
+                    <div className="comment-dropdown" onClick={handleReport}>
+                        <div className="comment-report-icon"></div>
+                        <div className="comment-report-text">신고하기</div>
                     </div>
-                    <div className='comment-dropdown' onClick={handleMsg}>
-                        <MdOutlineLocalPostOffice className='comment-msg-icon' />
-                        <div className='comment-msg-text'>쪽지 전송</div>
+                    <div className="comment-dropdown" onClick={handleMsg}>
+                        <MdOutlineLocalPostOffice className="comment-msg-icon" />
+                        <div className="comment-msg-text">쪽지 전송</div>
                     </div>
-                    <div className='comment-dropdown' onClick={handleDelete}>
-                        <div className='comment-delete-icon'></div>
-                        <div className='comment-delete-text'>삭제하기</div>
-                    </div>
+                    {showDeleteButton && (
+                        <div
+                            className="comment-dropdown"
+                            onClick={handleDelete}
+                        >
+                            <div className="comment-delete-icon"></div>
+                            <div className="comment-delete-text">삭제하기</div>
+                        </div>
+                    )}
                 </div>
             </div>
             {openReportModal && (
