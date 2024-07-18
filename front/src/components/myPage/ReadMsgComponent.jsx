88c9d3eb628aba5524b0ca59f229f7596039799e
hyeri-seo <141993427+hyeri-seo@users.noreply.github.com>
2024-07-18 11:17:55 +0900

    feat: 메시지 삭제 및 답장, 쪽지함 컴포넌트화

--- front/src/components/myPage/ReadMsgComponent.jsx
+++ front/src/components/myPage/ReadMsgComponent.jsx
@@ -2,21 +2,76 @@ import React, { useEffect, useState } from "react";
 import "../../assets/styles/App.scss";
 import { useSelector } from "react-redux";
 import { formatDate } from "../../util/DateUtil";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
+import { delSendMessage } from "../../api/myPageApi";
+import MsgModal from "../board/element/MsgModal";
 
-const ReadMsgComponent = ({ selectedMsg }) => {
+const initState = {
+    dtoList: [],
+    pageNumList: [],
+    pageRequestDTO: {
+        page: 0,
+        size: 10
+    },
+    prev: false,
+    next: false,
+    totalCount: 0,
+    prevPage: 0,
+    nextPage: 0,
+    totalPage: 0,
+    current: 0,
+};
+
+const ReadMsgComponent = ({ selectedMsg, deleteMessage }) => {
     const loginState = useSelector((state) => state.loginSlice);
+    const [msgData, setMsgData] = useState(initState);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const navigate = useNavigate();
 
     const handleMsgSend = (id) => {
-        console.log("Sending message");
+        setIsModalOpen(true);
     }
 
-    const handleMsgDelete = (id) => {
-        console.log("Deleting message with ID:", selectedMsg.id);
+    const handleMsgDelete = () => {
+        Swal.fire({
+            title: '쪽지를 삭제하시겠습니까?',
+            icon: "warning",
+            text: '삭제한 쪽지는 복구할 수 없습니다.',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: '확인',
+            cancelButtonText: '취소',
+        }).then((result) => {
+            if (result.isConfirmed) {
+                delSendMessage(selectedMsg.id).then(() => {
+                    Swal.fire({
+                        title: '삭제가 완료되었습니다.',
+                        icon: "success",
+                        confirmButtonColor: '#3085d6',
+                        confirmButtonText: '확인',
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            navigate("/myPage/msg/send");
+                        }
+                    });
+                    setMsgData((prevData) => ({
+                        ...prevData,
+                        dtoList: prevData.dtoList.filter((msg) => msg.id !== selectedMsg.id)
+                    }));
+                }).catch(err => {
+                    Swal.fire({
+                        title: '삭제 실패',
+                        icon: 'error',
+                        text: '잠시 후 다시 시도해주세요.',
+                        confirmButtonColor: '#3085d6',
+                        confirmButtonText: '확인'
+                    });
+                });
+            }
+        });
     };
-
-    const handleMsgList = (id) => {
-        console.log("Going back");
-    }
 
     const roleClassName = selectedMsg.role === "발신자" ? "sender" : "receiver";
 
@@ -30,7 +85,6 @@ const ReadMsgComponent = ({ selectedMsg }) => {
                     <div className="read-msg-author-info">
                         <div className="read-msg-author">
                             <div className={`read-msg-role ${roleClassName}`}>{selectedMsg.role}</div> | 
-                            {/* <img src={selectedMsg.profile} alt="프로필" className="read-msg-profile-image" /> */}
                             &nbsp; {selectedMsg.nickname} | {formatDate(selectedMsg.regDate)}
                         </div>
                     </div>
@@ -48,8 +102,14 @@ const ReadMsgComponent = ({ selectedMsg }) => {
             <div className="msg-button">
                 <button className="msg-send-button" onClick={handleMsgSend}>답장</button>
                 <button className="msg-delete-button" onClick={handleMsgDelete}>삭제</button>
-
             </div>
+            {isModalOpen && (
+                <MsgModal 
+                    senderId={loginState.id} 
+                    receiver={selectedMsg.nickname} 
+                    onClose={() => setIsModalOpen(false)} 
+                />
+            )}
         </>
     );
 };
--- front/src/components/myPage/element/MyReceiveMsgTableRowComponent.jsx
+++ front/src/components/myPage/element/MyReceiveMsgTableRowComponent.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { formatDate } from "../../../util/DateUtil";
+import MsgTableRowComponent from "./MyMsgTableRowComponent";
 
-const MyReceiveMsgTableRowComponent = ({ item, index, onClick }) => (
-    <tr className="board-row" onClick={onClick}>
-        <td>{index + 1}</td>
-        <td>{item.senderNickname}</td>
-        <td>{item.content}</td>
-        <td>{formatDate(item.regDate)}</td>
-        <td>삭제</td>
-    </tr>
+const MyReceiveMsgTableRowComponent = ({ item, index, onClick, onDelete }) => (
+    <MsgTableRowComponent 
+        item={item} 
+        index={index} 
+        onClick={onClick} 
+        onDelete={onDelete} 
+        role="send" 
+    />
 );
 
 export default MyReceiveMsgTableRowComponent;
