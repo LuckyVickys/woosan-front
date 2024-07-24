5fb004e04e0872cf854b964cb02ba58d5a4061c8
hyeri-seo <141993427+hyeri-seo@users.noreply.github.com>
2024-07-25 03:06:26 +0900

    feat: 메시지 전송에 토큰 추가

--- front/src/api/messageApi.js
+++ front/src/api/messageApi.js
@@ -3,9 +3,17 @@ import { API_SERVER_HOST } from "./boardApi";
 
 const prefix = `${API_SERVER_HOST}/api/message`;
 
-export const addMessage = async (formData) => {
+export const addMessage = async (formData, token) => {
   try {
-    const res = await axios.post(`${prefix}/add`, formData);
+    const res = await axios({
+      method: 'POST',
+      url: `${prefix}/add`,
+      data: formData,
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      }
+    });
     return res.data;
   } catch (error) {
     console.error(
