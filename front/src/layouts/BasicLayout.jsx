35db57971f2fc9bfac77c80d750b46484659216a
jidefine <youjio2000@gmail.com>
2024-07-26 00:36:38 +0900

    fix: 푸터 재수정

--- front/src/layouts/BasicLayout.jsx
+++ front/src/layouts/BasicLayout.jsx
@@ -6,6 +6,26 @@ import TopButton from "../components/common/TopButton";
 
 const BasicLayout = ({children}) => {
 
+    useEffect(() => {
+        adjustAreaHeight();
+        window.addEventListener('resize', adjustAreaHeight);
+
+        return () => {
+            window.removeEventListener('resize', adjustAreaHeight);
+        };
+    }, []);
+
+    const adjustAreaHeight = () => {
+        const windowHeight = window.innerHeight;
+        const headerHeight = document.querySelector('header').offsetHeight;
+        const navHeight = document.querySelector('.nav').offsetHeight;
+        const footerHeight = document.querySelector('footer').offsetHeight;
+        const area = document.querySelector('.area');
+
+        const areaHeight = windowHeight - headerHeight - navHeight - footerHeight;
+        area.style.minHeight = areaHeight + 'px';
+    };
+
     return (
         <div className="wrapper">
             <Header />
