ce62fa1c1ca284ef3b1cff8034dc29bb132a46f6
jidefine <youjio2000@gmail.com>
2024-07-26 00:19:27 +0900

    fix: 레이아웃 및 푸터 수정

--- front/src/layouts/BasicLayout.jsx
+++ front/src/layouts/BasicLayout.jsx
@@ -1,17 +1,19 @@
+import React, { useEffect } from 'react';
 import Header from "../components/common/Header";
 import Nav from "../components/common/Nav";
 import Footer from "../components/common/Footer";
 import TopButton from "../components/common/TopButton";
 
 const BasicLayout = ({children}) => {
+
     return (
         <div className="wrapper">
             <Header />
             <Nav />
             <div className="area">
                 {children}
-                <TopButton />
             </div>
+            <TopButton />
             <Footer />
         </div>
     );
